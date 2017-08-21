11aefa731c0920b97d9b3e376a3df5275050c537
pipixia <pipixia@pipi.com>
2017-08-21 22:01:43 +0800

    产品列表页面的头部和下拉框部分的静态页面完成

--- script/1.js
+++ script/1.js
@@ -4,7 +4,7 @@ $(function () {
     // 点击下拉
     $("#list_1").on("click", ".dropdown", function () {
         $(this).children(".dropdown-menu").fadeIn(1000);
-        $(this).stop(true, false).animate({ "margin-bottom": "138px" }, 800, function () {
+        $(this).stop(true, false).animate({ "margin-bottom": "140px" }, 800, function () {
             // 给标签添加标志，
             // 改变小箭头指向
             $(this).attr("flag", "true").find(".jiantou").html("&#xe501;");
@@ -18,4 +18,23 @@ $(function () {
             });
         }
     });
+
+
+
+
+
+
+
+    // 防止页面出现滚动条，先overflow:hidden
+    var myScroll;
+
+    function loaded() {
+        myScroll = new IScroll('#wrapper');
+    }
+
+    document.addEventListener('touchmove', function (e) { e.preventDefault(); }, isPassive() ? {
+        capture: false,
+        passive: false
+    } : false);
+    loaded();
 });
