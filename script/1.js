8754e806adac63ccf4d2aef897c79d0d78e6c840
pipixia <pipixia@pipi.com>
2017-08-21 20:21:48 +0800

    产品列表页面的头部和下拉框部分的静态页面完成

---
+++ script/1.js
@@ -0,0 +1,21 @@
+// 产品分页模块 - 慢慢买比价网
+
+$(function () {
+    // 点击下拉
+    $("#list_1").on("click", ".dropdown", function () {
+        $(this).children(".dropdown-menu").fadeIn(1000);
+        $(this).stop(true, false).animate({ "margin-bottom": "138px" }, 800, function () {
+            // 给标签添加标志，
+            // 改变小箭头指向
+            $(this).attr("flag", "true").find(".jiantou").html("&#xe501;");
+        });
+        // 链式编程
+    }).on("click", ".dropdown", function () {
+        if ($(this).attr("flag") == "true") {
+            $(this).children(".dropdown-menu").fadeOut(600);
+            $(this).stop(true, false).animate({ "margin-bottom": "0px" }, 800, function () {
+                $(this).attr("flag", "false").find(".jiantou").html("&#xf0170;");
+            });
+        }
+    });
+});
